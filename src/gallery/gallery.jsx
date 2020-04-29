65a95ec5ec55ef2b1c8a5d93bba913516eb8a9da
Derek Allen <derek@getweave.com>
2020-04-29 00:54:19 -0600

    added images and gallery

---
+++ src/gallery/gallery.jsx
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+exports.Gallery = function () {
+    return (<div>
+          <h1>Gallery</h1>
+      </div>);
+};
