97f9e203d3962bb09ebe765ccbd1f0a213a9977a
Derek Allen <derek@getweave.com>
2020-04-29 00:54:09 -0600

    added images and gallery

--- src/gallery/gallery.tsx
+++ src/gallery/gallery.tsx
@@ -1,9 +1,37 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {imagesArray} from './images/images';
 
 export const Gallery = () => {
+
+    const randomize = (array1: any) => {
+      let ctr = array1.length;
+      let temp;
+      let index;
+
+      while (ctr > 0) {
+          index = Math.floor(Math.random() * ctr);
+          ctr--;
+          temp = array1[ctr];
+          array1[ctr] = array1[index];
+          array1[index] = temp;
+      }
+      return array1;
+    };
+
   return (
       <div>
-          <h1>Gallery</h1>
+          <p>Dinner options</p>
+          <div style={{display: "flex", flexWrap: "wrap", order: 4, maxWidth: "1600px"}}>
+              {randomize(imagesArray.map((images, index) => {
+                  return (
+                      <div style={{display: "flex", flexDirection: "column"}}>
+                          <p>{images.name}</p>
+                          <img key={index} style={{padding: "0 20px 20px",objectFit: "cover", height: "300px", width: "300px"}} src={images.src} alt={images.name}/>
+                      </div>
+                    )
+                  }
+              ))}
+          </div>
       </div>
   )
 };
---
+++ src/gallery/images/images.ts
@@ -0,0 +1,58 @@
+export const imagesArray = [
+    {
+        name: 'Beef Burrito',
+        src: 'https://thumbor.thedailymeal.com/j1mjdpHyYmSG0NZHmTPt8GcFgnc=/870x565/https://www.thedailymeal.com/sites/default/files/recipe/2016/crop%20burrito%20shutterstock_282628049.jpg',
+    },
+    {
+        name: 'Lo Mein Steak',
+        src: 'https://therecipecritic.com/wp-content/uploads/2017/08/0C4A9579.jpg',
+    },
+    {
+        name: 'Brats',
+        src: 'https://www.johnsonville.com/dam/jcr:daa5f422-250d-4aa9-ba78-18c0a4a04cf7/grilled-italian-sausage-with-sweet-sour-peppers',
+    },
+    {
+        name: 'Chick-fil-A',
+        src: 'https://tastesbetterfromscratch.com/wp-content/uploads/2013/05/Chic-Fil-a-Sandwich-2-1.jpg',
+    },
+    {
+        name: 'Chicken Fajitas',
+        src: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chicken-fajitas-horizontal-jpg-1522721531.jpg?crop=1xw:1xh;center,top&resize=640:*',
+    },
+    {
+        name: 'Chicken fried rice',
+        src: 'https://www.cookingclassy.com/wp-content/uploads/2014/04/chicken-fried-rice-edit3+srgb.-500x375.jpg',
+    },
+    {
+        name: 'Hamburgers',
+        src: 'https://images-gmi-pmc.edge-generalmills.com/c9a2b6a5-60cc-4c4e-b456-f7f6f5a0ce04.jpg',
+    },
+    {
+        name: 'Jambalaya',
+        src: 'https://photos.bigoven.com/recipe/hero/shrimp-and-sausage-jambalaya-5f895f.jpg',
+    },
+    {
+        name: 'Liege Waffles',
+        src: 'https://ohsweetbasil.com/wp-content/uploads/2013/12/Waffle-Love-waffles-are-a-huge-hit-in-Utah-and-now-you-can-make-your-own-wafflelove-leige-waffles-from-home-ohsweetbasil.com-5.jpg',
+    },
+    {
+        name: 'Pizza',
+        src: 'https://www.cakenknife.com/wp-content/uploads/2016/04/Garlic-Agave-Pizza-Crust-Photograph-620x930.jpg',
+    },
+    {
+        name: 'Sunrise Chicken',
+        src: 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Sunshine-Chicken_EXPS_CHBZ19_3863_B10_24_12b-2-696x696.jpg',
+    },
+    {
+        name: 'Swedish Pancakes',
+        src: 'https://lilluna.com/wp-content/uploads/2017/10/swedish-pancakes-resize-5.jpg',
+    },
+    {
+        name: 'Taco Ring',
+        src: 'https://i.pinimg.com/originals/4f/df/6b/4fdf6b517af8f8b16612267d4a9a67c4.jpg',
+    },
+    {
+        name: 'Garlic breadsticks',
+        src: 'https://i1.wp.com/lmld.org/wp-content/uploads/2011/08/Garlic-Bread-Sticks-2.jpg?w=750&ssl=1',
+    },
+]
